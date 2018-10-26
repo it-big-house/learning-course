4e2ce9399d15350f4b05e439b7b13d450f83bc7a
world-wanted <future21star@yahoo.com>
2018-10-26 11:41:59 +0300

    #182 SingleChoiceComponent. Hide reveals if empty and set as innerHTML

--- src/app/play/brick/comp/comp_single_choice.component.ts
+++ src/app/play/brick/comp/comp_single_choice.component.ts
@@ -26,7 +26,10 @@ export class CompSingleChoice extends Comp {
                 <div fxFlex="1 0 0"></div>
                 <div fxLayout="column">
                     <div fittext [minFontSize]="15" [innerHTML]="choice"></div>
-                    <div *ngIf="attempt" class="reveal" fittext [minFontSize]="15">{{ data.data.reveals[getChoice(choice)] }}</div>
+                    <div *ngIf="attempt && data.data.reveals[getChoice(choice)]"
+                         class="reveal" fittext [minFontSize]="15"
+                         [innerHTML]="data.data.reveals[getChoice(choice)]">
+                    </div>
                 </div>
                 <div fxFlex="1 0 0"></div>
             </div>
